8c55f252762884c1599ab13bee76915bc9c11b6d
zhus1 <zhus1@CNENYEB1L1C.corp.emc.com>
2014-11-19 11:17:03 +0800

    Merged

--- LogParser/src/test/java/com/emc/prometheus/parser/dao/LogDaoTest.java
+++ LogParser/src/test/java/com/emc/prometheus/parser/dao/LogDaoTest.java
@@ -3,11 +3,13 @@ package com.emc.prometheus.parser.dao;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.emc.prometheus.parser.BaseTest;
+import com.emc.prometheus.parser.pojo.CompositeLogInfo;
 import com.emc.prometheus.parser.pojo.LogInfo;
 
 /** 
@@ -24,10 +26,25 @@ public class LogDaoTest  extends BaseTest{
 	public void testGetLogInfos() {
 		
 		
-		List<LogInfo> logInfos = logDao.getLogInfos();
+		 CompositeLogInfo logInfos = logDao.getLogInfos();
 		
-		for (LogInfo logInfo : logInfos) {
-			System.out.println(logInfo);
+		 
+		 List<LogInfo> logInfosFromGeninfo = logInfos.getLogInfos();
+		 
+		 System.out.println("==============From Geninfo");
+		 for (LogInfo logInfo : logInfosFromGeninfo) {
+			 System.out.println(logInfo);
+		}
+		 
+		 Map<LogInfo, List<LogInfo>> subLogMap = logInfos.getSubLogMap();
+		 if(subLogMap.isEmpty()){
+			 return ;
+		 }
+		 System.out.println("==============From SubContent");
+		 for (List<LogInfo> logInfo : subLogMap.values()) {
+			 for (LogInfo info : logInfo) {
+				 System.out.println(info);
+			}
 		}
 		
 	}
