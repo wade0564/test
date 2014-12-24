a8359f82adbeef74257fafef5987917d8bea0d16
991445760@qq.com <zhang667>
2014-12-24 14:08:55 +0800

    jcip

---
+++ study_test/src/main/java/cn/edu/ustc/jcip/Sequence.java
@@ -0,0 +1,18 @@
+package cn.edu.ustc.jcip;
+
+import net.jcip.annotations.*;
+
+/**
+ * Sequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@ThreadSafe
+public class Sequence {
+    @GuardedBy("this") private int nextValue;
+
+    public synchronized int getNext() {
+        return nextValue++;
+    }
+}
